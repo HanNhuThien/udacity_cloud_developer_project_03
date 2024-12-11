a24b2d565091fcb8da50ac4b382c1e54724e2ec8
nhungntc21 <nhung.ntc21@gmail.com>
2024-12-12 00:56:30 +0900

    refactor

---
+++ udagram-api-feed/src/sequelize.ts
@@ -0,0 +1,15 @@
+import {Sequelize} from 'sequelize-typescript';
+import {config} from './config/config';
+
+
+export const sequelize = new Sequelize(config.database, config.username, config.password,{
+  host: config.host,
+  dialect: 'postgres',
+  storage: ':memory:',
+  dialectOptions: {
+    ssl: {
+       require: true,
+       rejectUnauthorized: false
+    }
+ }
+});
