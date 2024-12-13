715eae145a4b3546f352705a011b61f25711ff45
nhungntc21 <nhung.ntc21@gmail.com>
2024-12-13 16:24:42 +0900

    50

--- udagram-api-user/src/sequelize.ts
+++ udagram-api-user/src/sequelize.ts
@@ -2,14 +2,18 @@ import {Sequelize} from 'sequelize-typescript';
 import {config} from './config/config';
 
 
-export const sequelize = new Sequelize(config.database, config.username, config.password,{
-  host: config.host,
-  dialect: 'postgres',
-  storage: ':memory:',
+export const sequelize = new Sequelize({
+  'username': config.username,
+  'password': config.password,
+  'database': config.database,
+  'host': config.host,
+
+  'dialect': "postgres",
+  'storage': ':memory:',
   dialectOptions: {
-    ssl: {
-       require: true,
-       rejectUnauthorized: false
-    }
- }
+   ssl: {
+      require: true,
+      rejectUnauthorized: false
+   }
+}
 });
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a5f6d09d1da4540d7861312a4edc41a8-2030214978.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
